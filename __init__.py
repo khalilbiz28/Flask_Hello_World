b1d9af538f0eb82452d4bb703a82c4690fdf6030
MassinissaBEY <104314110+MassinissaBEY@users.noreply.github.com>
2025-06-04 15:15:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ def exercices():
     return render_template('exercices.html')
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template("contact.html")
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)   #commit222
