a89aa9b71bab53b14cb75a553a0ddf32d2cb2ffa
khalilbiz28 <129905670+khalilbiz28@users.noreply.github.com>
2025-06-04 15:24:48 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ def hello_world():
 
 @app.route('/exercices/')
 def exercices():
-    return render_template('exercices.html')
+    return render_template('exercices.html') #Comm
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
