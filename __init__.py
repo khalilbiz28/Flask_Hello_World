2c9725764fc08f55d1fb3a5e2402d3be849c4121
MassinissaBEY <104314110+MassinissaBEY@users.noreply.github.com>
2025-06-04 15:51:47 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,6 +38,19 @@ def somme_multiple(nombres):
     except ValueError:
         return "<h2>Erreur : Veuillez entrer uniquement des nombres entiers dans l’URL.</h2>"
 
+@app.route('/valeur_maximale/', defaults={'nombres': ''})
+@app.route('/valeur_maximale/<path:nombres>')
+def valeur_maximale(nombres):
+    try:
+        # Convertir chaque nombre de l'URL en une liste d'entiers
+        liste = [int(n) for n in nombres.split('/') if n.strip() != '']
+        
+        # Trouver la valeur maximale
+        max_valeur = max(liste)
+        
+        return f"<h2>Les valeurs sont : {liste}</h2><p>La valeur maximale parmi ces valeurs est : {max_valeur}</p>"
+    except ValueError:
+        return "<h2>Erreur : Veuillez entrer uniquement des nombres entiers dans l’URL.</h2>"
 
 if __name__ == "__main__":
   app.run(debug=True)   #commit222
