50b24e83c52e58f429d0a626820b9833dedad07a
MassinissaBEY <104314110+MassinissaBEY@users.noreply.github.com>
2025-06-04 14:51:30 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,4 +14,4 @@ def exercices():
     return render_template('exercices.html')
                                                                                                                
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True)   #commit
